e44c8491010f84d89f9e5d7898cd19247f694870
Hiroshi MUTO <mutohiroshi0726@gmail.com>
2018-03-27 01:46:30 +0900

    :sparkles: article_viewの追加

---
+++ app/src/main/kotlin/com/example/mutouhiroshi/qiitaclient/model/Article.kt
@@ -0,0 +1,6 @@
+package com.example.mutouhiroshi.qiitaclient.model
+
+data class Article(val id: String,
+                   val title: String,
+                   val url: String,
+                   val user: User)
---
+++ app/src/main/kotlin/com/example/mutouhiroshi/qiitaclient/model/User.kt
@@ -0,0 +1,8 @@
+package com.example.mutouhiroshi.qiitaclient.model
+
+/**
+ * Created by mutouhiroshi on 2017/12/08.
+ */
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)
