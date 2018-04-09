142d3ecaefbe5cf39e2c0724593d850479f23df9
Hiroshi MUTO <mutohiroshi0726@gmail.com>
2018-04-10 01:39:44 +0900

    :sparkles: 記事リストの実装

--- app/src/main/kotlin/com/example/mutouhiroshi/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/com/example/mutouhiroshi/qiitaclient/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.mutouhiroshi.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import com.example.mutouhiroshi.qiitaclient.model.Article
+import com.example.mutouhiroshi.qiitaclient.model.User
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listAdapter.articles = listOf(dummyArticle("Kotlin入門", "たろう"),
+                dummyArticle("Java入門", "じろう"))
+
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        listView.adapter = listAdapter
+
     }
+
+    private fun dummyArticle(title: String, userName: String): Article =
+            Article(id = "",
+                    title = title,
+                    url = "https://kotlin.org/",
+                    user = User(id = "", name = userName, profileImageUrl = ""))
 }
