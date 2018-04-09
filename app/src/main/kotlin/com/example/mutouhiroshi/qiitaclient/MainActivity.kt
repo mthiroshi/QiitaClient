870f1a03bbb916bf29a98f9982bb0798cb0b345e
Hiroshi MUTO <mutohiroshi0726@gmail.com>
2018-04-10 02:46:19 +0900

    :sparkles: 記事詳細画面の実装

--- app/src/main/kotlin/com/example/mutouhiroshi/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/com/example/mutouhiroshi/qiitaclient/MainActivity.kt
@@ -18,12 +18,18 @@ class MainActivity : AppCompatActivity() {
 
         val listView: ListView = findViewById(R.id.list_view) as ListView
         listView.adapter = listAdapter
+        listView.setOnItemClickListener { adapterView, view, position, id ->
+            val article = listAdapter.articles[position]
+            ArticleActivity.intent(this, article).let { startActivity(it)}
+        }
 
     }
 
     private fun dummyArticle(title: String, userName: String): Article =
             Article(id = "",
                     title = title,
-                    url = "https://kotlin.org/",
+                    url = "https://kotlinlang.org/",
                     user = User(id = "", name = userName, profileImageUrl = ""))
+
+
 }
